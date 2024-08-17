8a4e45425726b5f62933d01292205de9c8dbe705
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-08-17 12:30:08 +0100

    Added down card deselect.

--- partypicker/Scenes/CardUI/CardStates/card_aiming_state.gd
+++ partypicker/Scenes/CardUI/CardStates/card_aiming_state.gd
@@ -1,6 +1,6 @@
 extends CardState
 
-const MOUSE_Y_SNAPBACK_THRESHOLD := 10
+const MOUSE_Y_SNAPBACK_THRESHOLD := 420 # check number works with scaling
 
 func enter() -> void:
 	card_ui.color.color = Color.WEB_PURPLE
@@ -22,7 +22,7 @@ func on_input(event: InputEvent) -> void:
 	var mouse_motion := event is InputEventMouseMotion
 	var mouse_at_bottom := card_ui.get_global_mouse_position().y > MOUSE_Y_SNAPBACK_THRESHOLD
 	
-	if event.is_action_pressed("right_mouse"):
+	if (mouse_motion and mouse_at_bottom) or event.is_action_pressed("right_mouse"):
 		print("Going to base")
 		transition_requested.emit(self, CardState.State.BASE)
 	elif event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse"):
