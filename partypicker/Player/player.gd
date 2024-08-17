e31026fd00823fd6d9ffc4803e825762a195f4f5
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-08-17 17:41:08 +0100

    Update player.gd

--- partypicker/Player/player.gd
+++ partypicker/Player/player.gd
@@ -1,19 +1,19 @@
 class_name Player
 extends Area2D
 
-@export var stats: Entity : set = set_enemy_stats
+@export var stats: Entity : set = set_player_stats
 
 @onready var sprite_2D: Sprite2D = $Sprite2D
 
 func _init() -> void:
 	print("EnemyRunning")
 
-func set_enemy_stats(value: Entity) -> void:
+func set_player_stats(value: Entity) -> void:
 	stats = value.create_instance()
 	print("SettingStats")
-	update_enemy()
+	update_player()
 
-func update_enemy() -> void:
+func update_player() -> void:
 	if not stats is Entity:
 		return
 	if not is_inside_tree():
