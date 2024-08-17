a2f387ab5b060e08afe3ebb3bf10ccffec6b5868
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-08-17 17:31:45 +0100

    Added PC.

---
+++ partypicker/Enemy/enemy.gd
@@ -0,0 +1,23 @@
+class_name Enemy
+extends Area2D
+
+@export var stats: Entity : set = set_enemy_stats
+
+@onready var sprite_2D: Sprite2D = $Sprite2D
+
+func _init() -> void:
+	print("EnemyRunning")
+
+func set_enemy_stats(value: Entity) -> void:
+	stats = value.create_instance()
+	print("SettingStats")
+	update_enemy()
+
+func update_enemy() -> void:
+	if not stats is Entity:
+		return
+	if not is_inside_tree():
+		await ready
+	
+	print("SettingSprite")
+	sprite_2D.texture = stats.art
--- partypicker/Scenes/BattleInstance/battle_instance.gd
+++ partypicker/Scenes/BattleInstance/battle_instance.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 # need to make this so that it is individual for each instance
-func _notification(what):
-	if what == NOTIFICATION_WM_MOUSE_ENTER:
-		print("enter")
-	elif what == NOTIFICATION_WM_MOUSE_EXIT:
-		print("exit")
+#func _notification(what):
+#	if what == NOTIFICATION_WM_MOUSE_ENTER:
+#		print("enter")
+#	elif what == NOTIFICATION_WM_MOUSE_EXIT:
+#		print("exit")
 		# Here include code to reset all cards into hand using the _on_card_ui_reparent_requested function in hand.gd
 
 var gen_deck = preload("res://characters/generic/generic_deck.tres")
@@ -20,9 +20,16 @@ func _init() -> void:
 	player  = Entity.new(gen_deck.cards)
 	enemy  = Entity.new(gen_deck.cards)
 
+#func create_instance() -> Resource:
+	#var enemy: Entity = 
+
 func _ready() -> void:
 	print(gen_deck.cards[0].name)
 	print(gen_deck.cards)
 	print(player.hand)
 	player.draw_card()
 	print(player.hand)
+	print(player.energy)
+
+func can_play_card(card: Card) -> bool:
+	return player.energy >= card.energy_cost
