0f2f9af340dcca4b61d7dcc82599b7b01b99fa40
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-08-17 14:19:51 +0100

    Added card things.

--- partypicker/custom_resources/card.gd
+++ partypicker/custom_resources/card.gd
@@ -5,10 +5,21 @@ enum Type {ATTACK, DEFEND, HEAL, SUPPORT}
 enum Target {LANE_SELF, LANE_ENEMY, LANE_ALL, LANE_CHOICE, ALL_ALLIES, ALL_ENEMIES, EVERYONE}
 
 @export_group("Card Attributes")
-@export var id: String
+@export var name: String
 @export var type: Type
 @export var target: Target
-@export var energy_cost: int
+
+var energy_cost: int
+var physical_damage: int
+var magical_damage: int
+var physical_block: int
+var magical_block: int
+var card_draw: int
+var card_discard: int
+var physical_dealt_increase: int
+var magical_dealt_increase: int
+var physical_taken_increase: int
+var magical_taken_increase: int
 
 func is_single_targeted() -> bool:
 	return target == Target.LANE_CHOICE
