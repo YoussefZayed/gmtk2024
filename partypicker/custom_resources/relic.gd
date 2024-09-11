6508e3b8a0a687bd0f4c9d07182b7ffe719dfabf
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-09-11 20:25:29 +0100

    Added initial class for relics.

---
+++ partypicker/custom_resources/relic.gd
@@ -0,0 +1,27 @@
+class_name Relic
+extends Resource
+
+enum Rarity {Common, Rare, Epic, Legendary, Cursed}
+
+@export var public_name: String = "Relic"
+@export var private_name: String = "Relic of X"
+@export var relic_type: String = "Relic"
+@export var expansion: String = "Basic"
+
+@export var energy_cost: int = 0
+@export var physical_damage: int = 0
+@export var magical_damage: int = 0
+@export var physical_block: int = 0
+@export var magical_block: int = 0
+@export var card_draw: int = 0
+@export var card_discard: int = 0
+@export var physical_dealt_increase: int = 0
+@export var magical_dealt_increase: int = 0
+@export var physical_taken_increase: int = 0
+@export var magical_taken_increase: int = 0
+@export var health_increase: int = 0
+@export var card_art: Texture
+@export_multiline var tooltip_text: String
+
+func identify() -> void:
+	public_name = private_name
